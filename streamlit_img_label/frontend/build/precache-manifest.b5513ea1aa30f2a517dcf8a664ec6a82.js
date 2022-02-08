142a3ad24b831e3b68645cd3a6bc9d5ba97a883b
Tianning Li <ltianningli@gmail.com>
2022-02-07 17:12:24 -0800

    add frontend build

---
+++ streamlit_img_label/frontend/build/precache-manifest.b5513ea1aa30f2a517dcf8a664ec6a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257cc5bae7213cd44281875035cb3db9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d6ee921a8ec95850ad44",
+    "url": "./static/css/main.00623a57.chunk.css"
+  },
+  {
+    "revision": "fe0995493108e32950ed",
+    "url": "./static/js/2.b19543fd.chunk.js"
+  },
+  {
+    "revision": "192ac78a6f5ee7863138d8750af26b49",
+    "url": "./static/js/2.b19543fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ee921a8ec95850ad44",
+    "url": "./static/js/main.25f4a86d.chunk.js"
+  },
+  {
+    "revision": "acc1e1b1767fa196d6fe",
+    "url": "./static/js/runtime-main.4a6ba311.js"
+  }
+]);
